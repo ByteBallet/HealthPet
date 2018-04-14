f93da6f3cdc799531c0e0c67ae6c984543fd7ea0
ByteBallet <shinystar0314@gmail.com>
2018-04-14 14:39:01 +0200

    Added fridge images and recycler adapter

--- android/src/com/gus/hackaton/MainActivity.java
+++ android/src/com/gus/hackaton/MainActivity.java
@@ -1,44 +1,53 @@
 package com.gus.hackaton;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static com.gus.hackaton.FridgeAdapter.COLUMNS_COUNT;
+
 public class MainActivity extends AppCompatActivity implements AndroidFragmentApplication.Callbacks {
 
-	@BindView(R.id.scan_barcode)
-	Button scanBarcode;
 
-	@Override
+    @BindView(R.id.fridgeRecyclerView)
+    RecyclerView recyclerView;
+
+    private FridgeAdapter fridgeAdapter;
+
+    @Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main_activity);
+
         ButterKnife.bind(this);
 
-		scanBarcode.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				Intent myIntent = new Intent(MainActivity.this, ScanActivity.class);
-				startActivity(myIntent);
-			}
-		});
 		getSupportFragmentManager()
 				.beginTransaction()
 				.replace(R.id.heroContainer, new HeroFragment())
 				.commit();
+
+		setupRecyclerView();
 	}
+
+    private void setupRecyclerView() {
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, COLUMNS_COUNT));
+
+        fridgeAdapter = new FridgeAdapter();
+        recyclerView.setAdapter(fridgeAdapter);
+    }
 
     @Override
     public void exit() {
